64d23aba986df4b5c15468506e0e27d99c5fccfd
meinasuck <142994701+meinasuck@users.noreply.github.com>
2023-12-06 17:21:14 +0100

    update link referral

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,11 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="footer">
-      <a href="https://github.com/meinasuck/weather-react" target="_blank">
+      <a
+        href="https://github.com/meinasuck/weather-react"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         {" "}
         Open source code
       </a>{" "}
